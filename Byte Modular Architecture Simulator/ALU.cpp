1481e15755b8338c80fe4c59af231dcbf9afc08c
NepSauce <greasyandmoist@gmail.com>
2025-07-16 18:06:03 -0300

    Fix: Fixed how the ALU functions.

--- Byte Modular Architecture Simulator/ALU.cpp
+++ Byte Modular Architecture Simulator/ALU.cpp
@@ -1,37 +1,45 @@
 #include "ALU.hpp"
 
-uint32_t ALU::execute(uint32_t inputA, uint32_t inputB, ALUOp op) {
-	switch (op) {
-		case ALUOp::ALU_OP_ADD:
-			return inputA + inputB;
-			break;
-		case ALUOp::ALU_OP_SUB:
-			return inputA - inputB;
-			break;
-		case ALUOp::ALU_OP_AND:
-			return inputA & inputB;
-			break;
-		case ALUOp::ALU_OP_OR:
-			return inputA | inputB;
-			break;
-		case ALUOp::ALU_OP_XOR:
-			return inputA ^ inputB;
-			break;
-		case ALUOp::ALU_OP_NOT:
-			return ~inputA;
-			break;
-		case ALUOp::ALU_OP_SHL:
-			return inputA << (inputB & 0x1F);
-			break;
-		case ALUOp::ALU_OP_SHR:
-			return inputA >> (inputB & 0x1F);
-			break;
-		case ALUOp::ALU_OP_SRA:
-			return static_cast<int32_t>(inputA) >> (inputB & 0x1F);
-			break;
-		case ALUOp::ALU_OP_SLT:
-			return (static_cast<int32_t>(inputA) < static_cast<int32_t>(inputB)) ? 1 : 0;
-			break;
-	}
+uint32_t ALU::execute(uint32_t a, uint32_t b, ALUOp op) {
+    uint32_t result = 0;
+
+    switch (op) {
+        case ALUOp::ALU_OP_ADD:
+            result = a + b;
+            break;
+        case ALUOp::ALU_OP_SUB:
+            result = a - b;
+            break;
+        case ALUOp::ALU_OP_AND:
+            result = a & b;
+            break;
+        case ALUOp::ALU_OP_OR:
+            result = a | b;
+            break;
+        case ALUOp::ALU_OP_XOR:
+            result = a ^ b;
+            break;
+        case ALUOp::ALU_OP_NOT:
+            result = ~a;  
+            break;
+        case ALUOp::ALU_OP_SHL:
+            result = a << (b & 0x1F);  
+            break;
+        case ALUOp::ALU_OP_SHR:
+            result = a >> (b & 0x1F);  
+            break;
+        case ALUOp::ALU_OP_SRA:
+            result = static_cast<int32_t>(a) >> (b & 0x1F);  
+            break;
+        case ALUOp::ALU_OP_SLT:
+            result = (static_cast<int32_t>(a) < static_cast<int32_t>(b)) ? 1 : 0;
+            break;
+    }
+    zero = (result == 0);
+
+    return result;
 }
 
+bool ALU::zeroFlag() const {
+    return zero;
+}
