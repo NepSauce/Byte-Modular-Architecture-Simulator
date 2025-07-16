f65c4c8b20886cb9783acf004639461597376a9a
NepSauce <greasyandmoist@gmail.com>
2025-07-16 17:59:38 -0300

    Feat: Finished ALU execute() function.

--- Byte Modular Architecture Simulator/ALU.cpp
+++ Byte Modular Architecture Simulator/ALU.cpp
@@ -7,7 +7,7 @@ uint32_t ALU::execute(uint32_t inputA, uint32_t inputB, ALUOp op) {
 			break;
 		case ALUOp::ALU_OP_SUB:
 			return inputA - inputB;
-			break;	
+			break;
 		case ALUOp::ALU_OP_AND:
 			return inputA & inputB;
 			break;
@@ -17,5 +17,21 @@ uint32_t ALU::execute(uint32_t inputA, uint32_t inputB, ALUOp op) {
 		case ALUOp::ALU_OP_XOR:
 			return inputA ^ inputB;
 			break;
+		case ALUOp::ALU_OP_NOT:
+			return ~inputA;
+			break;
+		case ALUOp::ALU_OP_SHL:
+			return inputA << (inputB & 0x1F);
+			break;
+		case ALUOp::ALU_OP_SHR:
+			return inputA >> (inputB & 0x1F);
+			break;
+		case ALUOp::ALU_OP_SRA:
+			return static_cast<int32_t>(inputA) >> (inputB & 0x1F);
+			break;
+		case ALUOp::ALU_OP_SLT:
+			return (static_cast<int32_t>(inputA) < static_cast<int32_t>(inputB)) ? 1 : 0;
+			break;
 	}
 }
+
